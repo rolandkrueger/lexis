e5f590d75a5d36ec451f4c14bc69580bf8b48361
Roland Krüger <info@rolandkrueger.info>
2018-12-02 11:07:50 +0100

    Implement default user input

---
+++ src/main/kotlin/org/jlexis/vocabulary/words/userinput/DefaultUserInput.kt
@@ -0,0 +1,13 @@
+package org.jlexis.vocabulary.words.userinput
+
+import org.jlexis.vocabulary.terms.Term
+
+class DefaultUserInput() : AbstractUserInput() {
+    val DEFAULT_TERM_KEY = RegisteredTermKey("DEFAULT_TERM")
+
+    var term : Term by UserInputTermPropertyDelegate(DEFAULT_TERM_KEY)
+
+    init {
+        registerTermKey(DEFAULT_TERM_KEY)
+    }
+}
