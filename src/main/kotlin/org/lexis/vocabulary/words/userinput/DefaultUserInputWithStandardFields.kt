32b35526e92171cb299985e4960bfdc800985f59
Roland Krüger <info@rolandkrueger.info>
2020-09-30 22:28:09 +0200

    Update user manual

--- src/main/kotlin/org/lexis/vocabulary/words/userinput/DefaultUserInputWithStandardFields.kt
+++ src/main/kotlin/org/lexis/vocabulary/words/userinput/DefaultUserInputWithStandardFields.kt
@@ -3,5 +3,6 @@ package org.lexis.vocabulary.words.userinput
 import org.lexis.vocabulary.words.userinput.standard.StandardUserInputDecorator
 import org.lexis.vocabulary.words.userinput.standard.StandardUserInputDecoratorImpl
 
-class DefaultUserInputWithStandardFields(userInputData: UserInputData = UserInputDataImpl(), keyPrefix: String) : DefaultUserInput by DefaultUserInputImpl(userInputData),
+class DefaultUserInputWithStandardFields(userInputData: UserInputData = UserInputDataImpl(), keyPrefix: String) :
+        DefaultUserInput by DefaultUserInputImpl(userInputData),
         StandardUserInputDecorator by StandardUserInputDecoratorImpl(keyPrefix, userInputData)
